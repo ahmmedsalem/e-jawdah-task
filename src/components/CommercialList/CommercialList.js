54443c4c1be0b0ccc69fea256c009b748cd4fd11
ahmeddsalem <ahmed_20160018@fci.helwan.edu.eg>
2023-01-01 09:23:43 +0200

    updated localization

--- src/components/CommercialList/CommercialList.js
+++ src/components/CommercialList/CommercialList.js
@@ -1,54 +1,57 @@
-import React from 'react';
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteRecord } from '../../redux/commercialsSlice';
-
+import { deleteRecord } from "../../redux/commercialsSlice";
+import { FormattedMessage } from "react-intl";
 import { AnimatePresence, motion } from "framer-motion";
-import "./CommercialList.scss"
-
+import "./CommercialList.scss";
 
 const CommercialList = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const recordsItems = useSelector((state) => state.records.items);
 
-    const recordsItems = useSelector((state) => state.records.items);
-
-    return ( 
-      <>
-      {recordsItems.length > 0 && 
+  return (
+    <>
+      {recordsItems.length > 0 && (
         <AnimatePresence>
-          {
-          recordsItems.map(item =>
-            <motion.div 
+          {recordsItems.map((item) => (
+            <motion.div
               key={item.id}
-              initial={{opacity: 0}}
-              animate={{opacity: 1}}
-              exit={{opacity: 0}}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
               className="col-6"
             >
               <div className="record-card p-15 py-20 mt-10 mb-5">
                 <div className="record-card__header d-flex">
                   <p className="text-white">
-                    الاسم التجاري الكامل
+                    <FormattedMessage
+                      id="commercialList.title"
+                      defaultMessage="الأسم التجاري الكامل"
+                    ></FormattedMessage>
                   </p>
-                  <button 
-                    className="record-card__button text-white mt-0 p-0" 
+                  <button
+                    className="record-card__button text-white mt-0 p-0"
                     onClick={() => dispatch(deleteRecord({ id: item.id }))}
                   >
-                    حذف
+                    <FormattedMessage
+                      id="button.delete"
+                      defaultMessage="حذف"
+                    ></FormattedMessage>
                     <i className="bi bi-x-lg m-5"></i>
                   </button>
                 </div>
                 <h5 className="record-card__content text-white">
-                  {item.commercialType} {item.commercialName} {item.commercialTypeFor}
+                  {item.commercialType} {item.commercialName}{" "}
+                  {item.commercialTypeFor}
                 </h5>
               </div>
             </motion.div>
-          )
-        }
+          ))}
         </AnimatePresence>
-        }
-        </>
-     );
-}
- 
+      )}
+    </>
+  );
+};
+
 export default CommercialList;
